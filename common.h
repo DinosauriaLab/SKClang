e335bb650d10dd3d961a467c3f9e9da137167e12
leoli <jafee201153@gmail.com>
2023-10-17 13:11:57 +0800

    * feat(common.h): add SQUARE macro for calculating the square of a number * feat(common.h): add SWAP macro for swapping two values * feat(common.h): add ARRAY_SIZE macro for calculating the size of an array * feat(common.h): add BIT macro for calculating the value of a bit at a given position * feat(common.h): add BIT_SET macro for setting a bit at a given position * feat(common.h): add BIT_CLEAR macro for clearing a bit at a given position

--- common.h
+++ common.h
@@ -14,12 +14,41 @@
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #endif
 
+#ifndef SQUARE
+#define SQUARE(a) ((a) * (a))
+#endif
+
 #ifndef ABS
 #define ABS(a) ((a) < 0 ? -(a) : (a))
 #endif
 
 #ifndef CLAMP
 #define CLAMP(a, min, max) (MAX(MIN(a, max), min))
+#endif
+
+#ifndef SWAP
+#define SWAP(a, b) \
+  do {             \
+    a ^= b;        \
+    b ^= a;        \
+    a ^= b;        \
+  } while (0)
+#endif
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#endif
+
+#ifndef BIT
+#define BIT(x) (1 << (x))
+#endif
+
+#ifndef BIT_SET
+#define BIT_SET(x, y) ((x) |= BIT(y))
+#endif
+
+#ifndef BIT_CLEAR
+#define BIT_CLEAR(x, y) ((x) &= ~BIT(y))
 #endif
 
 #define HEX2ASCII(x) ((x) < 10 ? (x) + '0' : (x)-10 + 'A')
